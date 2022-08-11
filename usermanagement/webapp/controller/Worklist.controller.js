34053335e412a273f1bec5be33a6644194c43fbd
Deepanjali Kumari <deepanjali.kumari@extentia.com>
2022-08-11 08:16:38 +0000

    activate/deactivate changes

--- usermanagement/webapp/controller/Worklist.controller.js
+++ usermanagement/webapp/controller/Worklist.controller.js
@@ -42,9 +42,15 @@ sap.ui.define(
              */
             onInit: function () {
                 var oRouter = this.getOwnerComponent().getRouter();
+                var oDataControl = {
+                    filterBar: {
+                        Search: ""
 
-                var objectModel = new JSONModel();
-                this.getView().setModel(objectModel, "objectModel");
+                    },
+                    PageBusy: false
+                };
+                var oViewCtrl = new JSONModel(oDataControl);
+                this.getView().setModel(oViewCtrl, "objectModel");
                 var oMdlCtrl = new JSONModel();
                 this.getView().setModel(oMdlCtrl, "oModelControl");
                 oRouter
@@ -54,12 +60,12 @@ sap.ui.define(
             _ResetFilterBar: function () {
                 var aCurrentFilterValues = [];
                 var aResetProp = {
-
+                    salesGroup: [],
+                    Search: ""
                 };
-                var oViewModel = this.getView().getModel("oModelControl");
+                var oViewModel = this.getView().getModel("objectModel");
                 oViewModel.setProperty("/filterBar", aResetProp);
-                var oTable = this.getView().byId("idUserTable");
-                oTable.rebindTable();
+
             },
             _onRouteMatched: function () {
                 this.getUserList();
@@ -67,8 +73,8 @@ sap.ui.define(
             },
             onPressAddObject: function () {
                 /*
-                 * Author: manik saluja
-                 * Date: 02-Dec-2021
+                 * Author: Deepanjali
+                 * Date: 11-AUg-2022
                  * Language:  JS
                  * Purpose: Navigation to add object view and controller
                  */
@@ -77,8 +83,8 @@ sap.ui.define(
             },
             _InitData: function () {
                 /*
-                 * Author: manik saluja
-                 * Date: 02-Dec-2021
+                 * Author:Deepanjali
+                 * Date: 11-AUg-2022
                  * Language:  JS
                  * Purpose: once the view elements load we have to 
                  * 1. get the logged in users informaion. 2.rebind the table to load data and apply filters 3. perform other operations that are required at the time 
@@ -94,8 +100,8 @@ sap.ui.define(
             },
             _addSearchFieldAssociationToFB: function () {
                 /*
-                 * Author: manik saluja
-                 * Date: 02-Dec-2021
+                 * Author: Deepanjali
+                 * Date: 11-AUg-2022
                  * Language:  JS
                  * Purpose: add the search field in the filter bar in the view.
                  */
@@ -107,7 +113,7 @@ sap.ui.define(
                 if (!oSearchField) {
                     // @ts-ignore
                     oBasicSearch = new sap.m.SearchField({
-                        value: "{oSearchControl>/filterBar/Search}",
+                        value: "{objectModel>/filterBar/Search}",
                         showSearchButton: true,
                         search: othat.onSearch.bind(othat)
                     });
@@ -117,17 +123,18 @@ sap.ui.define(
                 return promise;
             },
 
-            getUserList: function (aTop, skip, sSearch, salesgrp, appstaus, ssort, sortcolumn) {
+            getUserList: function (nTopRec, nskipRec, sSearch, aSalesgrp, appstaus, ssort, sortcolumn) {
                 var oModel = this.getOwnerComponent().getModel();
                 this.getView().getModel("objectModel").setProperty("/PageBusy", true);
                 var oActionODataContextBinding = oModel.bindContext("/getTSIUserList(...)");
+                oActionODataContextBinding.setParameter("topRec", nTopRec ? nTopRec : 5);
+                oActionODataContextBinding.setParameter("skipRec", nskipRec ? nskipRec : 0);
                 oActionODataContextBinding.setParameter("searchText", sSearch ? sSearch : "");
-                oActionODataContextBinding.setParameter("salesGroup", "");
-                oActionODataContextBinding.setParameter("appStatus", null);
+                oActionODataContextBinding.setParameter("salesGroup", aSalesgrp ? aSalesgrp : []);
+                oActionODataContextBinding.setParameter("appStatus", appstaus ? appstaus : null);
                 oActionODataContextBinding.setParameter("sortOrder", ssort ? ssort : "DESC");
                 oActionODataContextBinding.setParameter("sortColumn", sortcolumn ? sortcolumn : "FIRST_NAME");
-                oActionODataContextBinding.setParameter("topRec", null);
-                oActionODataContextBinding.setParameter("skipRec", null);
+
                 oActionODataContextBinding.execute().then(
                     function () {
                         this.getView().getModel("objectModel").setProperty("/PageBusy", false);
@@ -139,13 +146,15 @@ sap.ui.define(
                 );
             },
             onSearch: function (oevent) {
-                var sText =oevent.getParameters().query;
-                this.getUserList(null, null, sText, null, null, "ASC", "FIRST_NAME");
+
+                var sSearchText = this.getView().getModel("objectModel").getProperty("/filterBar/Search");
+                var aSaleGrp = this.getView().getModel("objectModel").getProperty("/filterBar/salesGroup");
+                var sApprvlStatus = this.getView().getModel("objectModel").getProperty("/filterBar/ApprovalStatus");
+                this.getUserList(null, null, sSearchText, aSaleGrp, sApprvlStatus, "DESC", "FIRST_NAME");
             },
             onSort: function () {
                 this.getUserList(null, null, null, null, null, "ASC", "FIRST_NAME");
             },
-
             onDataExport: sap.m.Table.prototype.exportData || function () {
                 var oModel = this.getView().getModel("oModelControl");
                 var oExport = new Export({
@@ -208,8 +217,6 @@ sap.ui.define(
                     oExport.destroy();
                 });
             },
-
-
             oProdValueHelpRequest: function () {
                 var oView = this.getView()
                 var othat = this;
@@ -226,35 +233,38 @@ sap.ui.define(
                 }
             },
 
-
-
-
             onResetFilterBar: function () {
                 this._ResetFilterBar();
+                this.getUserList(5, 0);
             },
             onPressApproveReject: function (oEve) {
-                var sEmail = oEve.getSource().getModel("oModelControl").getData();
-                sButton = oEve.getSource().getTooltip().trim().toLowerCase(),
-                    sStatus = sButton === "accepted" ? "2" : "rejected" ? "3" : "1",
-                    sMessage = sButton === "accepted" ? "Approve" : "Reject",
-                    sAccptRejctCheck = sButton === "accepted" ? this._showMessageBox("information", "MsgConfirm", [sMessage], this.onApproveRejectServiceCall.bind(this, sEmail, sStatus, "Approved")) : this._showMessageBox("remark", "MsgConfirm", [sMessage], "", "", sEmail, sStatus);
-                // this._showMessageBox("information", "MsgConfirm", [sMessage], this.onApproveRejectServiceCall.bind(this, iId, sStatus));
+                var sEmail = oEve.getSource().getBindingContext("oModelControl").getObject().EMAIL;
+                var sActivated = oEve.getSource().getBindingContext("oModelControl").getObject().IS_ACTIVATED;
+                var sStatus = sActivated === 0 ?  1  : 0,
+                sMessage = sActivated === 1 ? "Deactivate" : "Activate",
+                sAccptRejctCheck = sActivated === "Deactivated" ? this._showMessageBox("information", "MsgConfirm", [sMessage], this.onApproveRejectServiceCall.bind(this, sEmail, sStatus, "Approved")) : this._showMessageBox("information", "MsgConfirm", [sMessage], this.onApproveRejectServiceCall.bind(this, sEmail, sStatus, "Approved"));
+               
             },
+           onApproveRejectServiceCall: function (sEmail, sStatus) {
+               
+                var oModel = this.getOwnerComponent().getModel();
+                this.getView().getModel("objectModel").setProperty("/PageBusy", true);
+                var oActionODataContextBinding = oModel.bindContext("/updateTSIUserStatus(...)");
+                oActionODataContextBinding.setParameter("email", sEmail);
+                oActionODataContextBinding.setParameter("isActivated", sStatus);
+               
 
-            onApproveRejectServiceCall: function (sEmail, sStatus) {
-                var oPayLoad = {
-                    "email": sEmail,
-                    "isActivated": sStatus
-                };
-                var oDataModel = this.getView().getModel();
-                oDataModel.update(`/ContractorReassignmentRequests(${iId})/ReassignmentStatusId`, oPayLoad, {
-                    success: function (data) {
-                        var oTable = this.getView().byId("idWorkListTable1");
-                        oTable.rebindTable();
-                    }.bind(this),
-                    error: function (data) {
-                    }.bind(this),
-                });
+                oActionODataContextBinding.execute().then(
+                    function () {
+                        this.getUserList();
+                        this.getView().getModel("objectModel").setProperty("/PageBusy", false);
+                        var oResponseTxt = oActionODataContextBinding.getBoundContext().getObject();
+                        MessageToast.show(oResponseTxt.value);
+                        
+                       
+                    }.bind(this)
+                );
+                
             },
         }
         );
